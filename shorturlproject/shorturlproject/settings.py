a04a5d50de4c2fd6ee98d4eefa585d546c0427a1
akshay <akshaybisht001@gmail.com>
2022-09-04 23:08:43 +0530

    heroku

--- shorturlproject/shorturlproject/settings.py
+++ shorturlproject/shorturlproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, "static")
+]
 
 django_heroku.settings(locals())
